ab585a8158343934427c56947dceca5c1bdebc59
Eduardo Vieira <bolacha0953@gmail.com>
2022-11-24 11:54:46 +0100

    Update pregunta4.py

--- pregunta4.py
+++ pregunta4.py
@@ -11,9 +11,31 @@ def limpiarConsola():
         os.system("cls")
     else:
         os.system("clear")
+    return 0
+
+def matrizTexto(matriz):
+    linea1 = linea2 = linea3 = " "
+    for x in range(4):
+        linea1 += str(matriz[x][0]) + " "
+    for x in range(4):
+        linea2 += str(matriz[x][1]) + " "
+    for x in range(4):
+        linea3 += str(matriz[x][2]) + " "
+    lineaFinal = str(linea1 + "\n" + linea2 + "\n" + linea3)
+    return lineaFinal
+
+def requestMatriz(matriz):
+    for x in range(4):
+        for y in range(3):
+            matriz[x].append(int(input("Numero entero: ")))
+            limpiarConsola()
+    return matrizTexto(matriz)
 
 
-for x in range(4):
-    for y in range(3):
-        matrizA[x].append(int(input("Numero entero: ")))
-print(matrizA)
+if __name__ == "__main__":
+    limpiarConsola()
+    print(requestMatriz(matrizA))
+    input("Pulse cualquier botón para continuar con la matriz B")
+    print(requestMatriz(matrizB))
+    input("Pulse cualquier botón para continuar con la suma\n")
+    input("")
